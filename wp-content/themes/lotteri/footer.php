c3a5881d80d9bf5627ca46aec04743125bd65f92
Gaston <briones.gaston@gmail.com>
2016-10-03 12:53:23 -0300

    add copy

--- wp-content/themes/lotteri/footer.php
+++ wp-content/themes/lotteri/footer.php
@@ -1,4 +1,7 @@
 <footer id="footer">
+    <div class="copy">
+        2016 ® Patricia Gabriel . Todos los derechos reservados | Diseño Web: <a href="http://swapdg.com/">SWAP design</a>
+    </div>
     <?php wp_footer(); ?>
     <script src="<?php echo get_template_directory_uri(); ?>/js/libs/jquery-2.1.3.min.js"></script>
     <script src="<?php echo get_template_directory_uri(); ?>/js/fancybox/jquery.fancybox.pack.js"></script>
