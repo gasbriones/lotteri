de314dc4c7b50455953ba694b380a66f04ca4c1f
Gaston <briones.gaston@gmail.com>
2016-10-03 13:06:34 -0300

    goocle convertion form

--- wp-content/themes/lotteri/ajax/newsletter.php
+++ wp-content/themes/lotteri/ajax/newsletter.php
@@ -32,9 +32,23 @@ $headers .= "Content-type: text/html; charset=UTF-8\r\n";
 $headers .= "From: $nombre  <$email>\r\n";
 
 mail($destinatario,$asunto,$cuerpo,$headers);
+?>
 
-
-
-00.
-12102}}
-                6t6t rddd
+<!-- Google Code for Contacto Conversion Page -->
+<script type="text/javascript">
+/* <![CDATA[ */
+var google_conversion_id = 874866320;
+var google_conversion_language = "en";
+var google_conversion_format = "3";
+var google_conversion_color = "ffffff";
+var google_conversion_label = "QuUPCKve3GkQkM2VoQM";
+var google_remarketing_only = false;
+/* ]]> */
+</script>
+<script type="text/javascript" src="//www.googleadservices.com/pagead/conversion.js">
+</script>
+<noscript>
+<div style="display:inline;">
+<img height="1" width="1" style="border-style:none;" alt="" src="//www.googleadservices.com/pagead/conversion/874866320/?label=QuUPCKve3GkQkM2VoQM&amp;guid=ON&amp;script=0"/>
+</div>
+</noscript>
